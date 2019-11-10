05223eb18fd8fabaaabaeccf028ae80b5f4d90b1
ranwahle <ran.wahle@gmail.com>
2019-11-10 17:31:58 +0200

    Create tables error handling

--- data-access.js
+++ data-access.js
@@ -14,8 +14,8 @@ CREATE TABLE "Items" (
     id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "ItemName" text
 );
-
-CREATE UNIQUE INDEX "Items_pkey" ON "Items"(id int4_ops);
+if not exists "Items_pkey"
+  CREATE UNIQUE INDEX "Items_pkey" ON "Items"(id int4_ops);
 `;
 
   const selectedItemsTable = `
